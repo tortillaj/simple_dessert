50b632e89582ba1cfcc1b5dc1825f26d3f787d96
James Cole <jcole@newsignature.com>
2013-03-07 15:54:44 -0700

    remote the junk

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dessert::Application.config.secret_token = '56081691a3e3147c0ed5e8552c3bb210b98e0201116d464913a46023cd565c6afd3f8fc069cd6062bb250d5b904795bb108075feca2e6794adc41e29d03e36b9'
--- config/mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app12999661@heroku.com',
-  :password       => 'Marvin#7131',
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
